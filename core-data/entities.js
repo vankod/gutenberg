15cdb930a238f217ffbe03fcc48ed627d76dc36b
Riad Benguella <benguella@gmail.com>
2018-05-08 13:38:27 +0100

    Move LoadKindEntities to the entities file

--- core-data/entities.js
+++ core-data/entities.js
@@ -1,12 +1,18 @@
 /**
  * External dependencies
  */
-import { upperFirst, camelCase, map } from 'lodash';
+import { upperFirst, camelCase, map, find } from 'lodash';
 
 /**
  * WordPress dependencies
  */
 import apiRequest from '@wordpress/api-request';
+
+/**
+ * Internal dependencies
+ */
+import { hasEntitiesByKind } from './selectors';
+import { addEntities } from './actions';
 
 export const defaultEntities = [
 	{ name: 'postType', kind: 'root', key: 'slug', baseUrl: '/wp/v2/types' },
@@ -46,3 +52,25 @@ export const getMethodName = ( kind, name ) => {
 	const nameSuffix = upperFirst( camelCase( name ) );
 	return `get${ kindPrefix }${ nameSuffix }`;
 };
+
+/**
+ * Loads the kind entities into the store.
+ *
+ * @param {Object} state Global state
+ * @param {string} kind  Kind
+ */
+export async function* loadKindEntities( state, kind ) {
+	const hasEntities = hasEntitiesByKind( state, kind );
+
+	if ( hasEntities ) {
+		return;
+	}
+
+	const kindConfig = find( kinds, { name: kind } );
+	if ( ! kindConfig ) {
+		return;
+	}
+
+	const entities = await kindConfig.loadEntities();
+	yield addEntities( entities );
+}
